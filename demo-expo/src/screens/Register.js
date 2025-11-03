59b012234aa27f8ccf40646a9873ea9423d8bf8b
MilagrosGalarce <galarcem@udesa.edu.ar>
2025-11-03 18:25:17 -0300

    arranque con el register

--- demo-expo/src/screens/Register.js
+++ demo-expo/src/screens/Register.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 
-export class Register extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
+export default class Register extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            username: '',
+            password: '',
+            email: '',
+            error: ''
+        }
+    }
 
-export default Register
+    submit(username, email, password){
+        console.log('Creando usuario con los valores', { username, email, password })
+
+        // Validaciones básicas
+        if(username.length < 3){
+            this.setState({ error: 'El nombre de usuario debe tener al menos 3 caracteres.' })
+            return
+        }
+        if(!email.includes('@')){
+            this.setState({ error: 'El email no es válido.' })
+            return
+        }
+        if(password.length < 6){
+            this.setState({ error: 'La contraseña debe tener al menos 6 caracteres.' })
+            return
+        }}}
