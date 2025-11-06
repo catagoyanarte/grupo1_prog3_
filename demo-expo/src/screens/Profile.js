bb20ab8fd3c8670bea630fe0b691071398b97cd9
MilagrosGalarce <galarcem@udesa.edu.ar>
2025-11-05 21:07:17 -0300

    avance con el perfil asi se puede mostrar el email y usuario logueado

--- demo-expo/src/screens/Profile.js
+++ demo-expo/src/screens/Profile.js
@@ -10,6 +10,72 @@ export class Profile extends Component {
       loadingUsuario: true,
       loadingPosteos: true
     }
-  }}
+  }
+  
+
+  componentDidMount() {
+    console.log(auth.currentUser)
+    db.collection("users").where("owner", "==", auth.currentUser.email).onSnapshot(data => {
+      data.forEach(doc => {
+        this.setState({ usuario: doc.data(), loadingUsuario: false })
+      })
+    })
+
+
+    db.collection("posts").where("owner", "==", auth.currentUser.email).onSnapshot(
+      docs => {
+        let posts = [];
+        docs.forEach(doc => {
+          posts.push({
+            id: doc.id,
+            data: doc.data()
+          })
+        })
+        posts.sort((a,b)=>b.data.createdAt-a.data.createdAt) //ordeno de mas nuevo a mas viejo
+        console.log(posts)
+         this.setState({
+            posteos: posts,
+            loadingPosteos: false
+          })
+      })
+    }
+
+
+  render() {
+      return(
+      <View>
+    {
+      this.state.loadingUsuario && this.state.loadingPosteos ? <Text>Cargando</Text> :
+        <View>
+          <Text>Profile</Text>
+          <Text>email: {this.state.usuario.owner}</Text>
+          <Text>usuario: {this.state.usuario.username}</Text>
+          
+          <FlatList
+          data={this.state.posteos}
+          keyExtractor={(item)=> item.id.toString()}
+          renderItem={(item)=>{
+            console.log(item)
+            return(
+<View>
+            <Text>{item.item.data.owner}</Text>
+            <Text>{item.item.data.texto}</Text>
+            <Text>----------------</Text>
+          </View>
+            )
+          } }
+          />
+        </View>
+    }
+      </View >
+    )
+  }
+}
+
+
+export default Profile
+
+
+
 
 
